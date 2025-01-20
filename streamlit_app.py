5af9d1c6af6838b76303bcadf2293d1068aca58b
Ahmad Ammari <ammariect@gmail.com>
2025-01-20 12:10:38 +1100

    added llama3.2 in ollama

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.title("LLM Chat App")
 # Sidebar for model selection
 with st.sidebar:
     st.markdown("# Chat Options")
-    model = st.selectbox('What model would you like to use?', ['phi4', 'aws-nova-micro'])
+    model = st.selectbox('What model would you like to use?', ['llama3.2', 'phi4', 'aws-nova-micro'])
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -48,7 +48,7 @@ if user_prompt:
 
     # Generate response from LLM
     with st.spinner('Generating response...'):
-        if model == 'phi4':
+        if model in ['llama3.2', 'phi4']:
             response = ollama.chat(model=model, messages=[{"role": "user", "content": full_prompt}])
             response_content = response['message']['content']
         elif model == 'aws-nova-micro':
